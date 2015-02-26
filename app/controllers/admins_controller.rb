8a35a4181cd7eb67ed2d606664d1f0a2a4c4b067
Dennis Zografos <dzografos@skillz.com>
2015-02-26 11:50:19 -0500

    very basic summing of votes on admin section

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,59 @@
 class AdminsController < ApplicationController
+
+  def index
+    if(!current_admin)
+      return
+    end
+
+    @results = Hash.new
+
+    @grant_submissions = GrantSubmission.where(grant_id: [1,2]) #lace and temple only for now
+
+    @grant_submissions.each do |gs|
+
+      votes = Vote.where("grant_submission_id = ?",gs.id)
+
+      t_sum = 0
+      t_num = 0;
+      c_sum = 0;
+      c_num = 0;
+      f_sum = 0;
+      f_num = 0;
+
+      votes.each do |gsv|
+        if(gsv.score_t)
+          t_sum = t_sum+gsv.score_t
+          t_num = t_num+1
+        end
+
+        if(gsv.score_c)
+          c_sum = c_sum+gsv.score_c
+          c_num = c_num+1
+        end
+
+        if(gsv.score_f)
+          f_sum = f_sum+gsv.score_f
+          f_num = f_num+1
+        end
+
+      end
+
+      @results[gs.id] = Hash.new
+
+      @results[gs.id]['num_t'] = t_num
+      @results[gs.id]['sum_t'] = t_sum
+
+      @results[gs.id]['num_c'] = c_num
+      @results[gs.id]['sum_c'] = c_sum
+
+      @results[gs.id]['num_f'] = f_num
+      @results[gs.id]['sum_f'] = f_sum
+
+
+      #@votes[gs.id] = vote
+
+    end
+
+
+  end
 end
